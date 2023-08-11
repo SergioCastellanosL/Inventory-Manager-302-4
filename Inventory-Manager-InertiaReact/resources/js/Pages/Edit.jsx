7150307590401a8f350936e44453eacf1b1f63fa
Minaa2022 <amin0057@algonquinlive.com>
2023-08-11 15:45:13 -0400

    last edit form updated

--- Inventory-Manager-InertiaReact/resources/js/Pages/Edit.jsx
+++ Inventory-Manager-InertiaReact/resources/js/Pages/Edit.jsx
@@ -3,25 +3,25 @@ import { Head } from "@inertiajs/react";
 import { useState, useEffect, useSearchParams } from "react";
 
 export default function Edit({ auth, ...props }) {
-    const [data, setData] = useState([]); 
+    const [data, setData] = useState([]);
     const [selection, setSelection] = useState();
     const [item, setItem] = useState([]);
-    const type= props.type;
-    const id= props.id;
+    const type = props.type;
+    const id = props.id;
     const getItem = () => {
         /* fetch API in action */
         fetch(`http://127.0.0.1:8000/api/items/${data.item_id}`, {
-            method: 'GET',
+            method: "GET",
         })
-            .then(response => {
+            .then((response) => {
                 return response.json();
             })
-            .then(items => {
-                //Fetched product is stored in the state 
+            .then((items) => {
+                //Fetched product is stored in the state
                 setItem(items);
                 console.log(item.price, data.amount);
-                let total= data.amount * items.price;
-                setData({ ...data, total_price: total })
+                let total = data.amount * items.price;
+                setData({ ...data, total_price: total });
             });
     };
     const deleteEvent = (id) => {
@@ -38,83 +38,83 @@ export default function Edit({ auth, ...props }) {
         /* fetch API in action */
         console.log(props);
         let url = `http://127.0.0.1:8000/api/${props.type}s/${props.id}`;
-        if(props.type == "invoice"){
+        if (props.type == "invoice") {
             console.log(props.typeTwo);
-            if(props.typeTwo == "client"){
+            if (props.typeTwo == "client") {
                 setSelection("Client");
                 url = `http://127.0.0.1:8000/api/client_${props.type}s/${props.id}`;
-            }else if(props.typeTwo == "provider"){
+            } else if (props.typeTwo == "provider") {
                 setSelection("Provider");
                 url = `http://127.0.0.1:8000/api/provider_${props.type}s/${props.id}`;
             }
         }
-        fetch(url,{
-            method: 'GET',
+        fetch(url, {
+            method: "GET",
         })
-        .then(response => {
-            return response.json();
-        })
+            .then((response) => {
+                return response.json();
+            })
             .then((data) => {
                 //Fetched product is stored in the state
                 setData(data);
             });
     };
 
-    const handleChange = (event)=> {
-        if(event.target.name == "select"){
+    const handleChange = (event) => {
+        if (event.target.name == "select") {
             setSelection(event.target.value);
-        }else{
+        } else {
             setData({ ...data, [event.target.name]: event.target.value });
         }
-      }
-    const handleSubmit = ()=>{
+    };
+    const handleSubmit = () => {
         let url = `http://127.0.0.1:8000/api/${props.type}s/${props.id}`;
-        if(props.type == "invoice"){
-            if(selection == "Client"){
+        if (props.type == "invoice") {
+            if (selection == "Client") {
                 url = `http://127.0.0.1:8000/api/client_${props.type}s/${props.id}`;
-            }else if(selection == "Provider"){
+            } else if (selection == "Provider") {
                 url = `http://127.0.0.1:8000/api/provider_${props.type}s/${props.id}`;
             }
         }
         console.log(url);
         console.log(data);
         try {
-            fetch(url,{
-                method: 'PUT',
+            fetch(url, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
-                  },
-                body: JSON.stringify(data)
+                },
+                body: JSON.stringify(data),
             })
-            .then(response => {
-                console.log(response);
-                return response.json();
-            })
-            .then(data => {
-                //Fetched product is stored in the state
-                const loc = props.type + "s";
-                if(selection == "Client"){
-                    window.location.href = route(loc,{type:"client",id: -100});
-                }else if(selection == "Provider"){
-                    console.log("provider route");
-                    window.location.href = route(loc) +"?type=provider";;
-                }else{
-                    window.location.href = route(loc);
-                }
-            });
-        } catch (error) {
-            
-        }
-        
-    }
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    //Fetched product is stored in the state
+                    const loc = props.type + "s";
+                    if (selection == "Client") {
+                        window.location.href = route(loc, {
+                            type: "client",
+                            id: -100,
+                        });
+                    } else if (selection == "Provider") {
+                        console.log("provider route");
+                        window.location.href = route(loc) + "?type=provider";
+                    } else {
+                        window.location.href = route(loc);
+                    }
+                });
+        } catch (error) {}
+    };
     useEffect(() => {
-        if(data.item_id && data.amount && props.type =="invoice"){
+        if (data.item_id && data.amount && props.type == "invoice") {
             getItem();
-        }else if(!data.id){
+        } else if (!data.id) {
             console.log("datas");
             getData();
         }
-     },[data.amount, data.item_id]); 
+    }, [data.amount, data.item_id]);
 
     return (
         <AuthenticatedLayout
@@ -236,54 +236,135 @@ export default function Edit({ auth, ...props }) {
                                     />
                                 </div>
                             ) : null}
-                            {type == "invoice" ?
-                                <div>
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex justify-center items-center">
                                     <fieldset>
-                                        <legend>Invoice Type:</legend>
+                                        <legend className="mb-4 w-200 flex justify-center items-center">
+                                            Invoice Type:
+                                        </legend>
                                         <div>
-                                            {selection=="Client"?<input type="radio" name="select" value="Client" onChange={handleChange} checked />:<input type="radio" name="select" value="Client" onChange={handleChange} />}
-                                            <label >Client</label>
+                                            {selection == "Client" ? (
+                                                <input
+                                                    type="radio"
+                                                    name="select"
+                                                    value="Client"
+                                                    onChange={handleChange}
+                                                    checked
+                                                />
+                                            ) : (
+                                                <input
+                                                    type="radio"
+                                                    name="select"
+                                                    value="Client"
+                                                    onChange={handleChange}
+                                                />
+                                            )}
+                                            <label>Client</label>
                                         </div>
                                         <div>
-                                        {selection=="Provider"?<input type="radio" name="select" value="Provider" onChange={handleChange} checked />:<input type="radio" name="select" value="Provider" onChange={handleChange} />}
-                                            <label >Provider</label>
+                                            {selection == "Provider" ? (
+                                                <input
+                                                    type="radio"
+                                                    name="select"
+                                                    value="Provider"
+                                                    onChange={handleChange}
+                                                    checked
+                                                />
+                                            ) : (
+                                                <input
+                                                    type="radio"
+                                                    name="select"
+                                                    value="Provider"
+                                                    onChange={handleChange}
+                                                />
+                                            )}
+                                            <label>Provider</label>
                                         </div>
                                     </fieldset>
                                 </div>
-                                : null}
-                            {type == "invoice" ?
-                                <div>
-                                    <label>{selection} Id: </label>
-                                    {selection =="Client" ? <input type="number" name="client_id" value={data.client_id} onChange={handleChange} /> : null}
-                                    {selection =="Provider" ? <input type="number" name="provider_id" value={data.provider_id} onChange={handleChange} /> : null}
+                            ) : null}
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex justify-center items-center">
+                                    <label className="block text-gray-700 mr-3  w-24 text-right">
+                                        {selection} Id:{" "}
+                                    </label>
+                                    {selection == "Client" ? (
+                                        <input
+                                            type="number"
+                                            name="client_id"
+                                            value={data.client_id}
+                                            onChange={handleChange}
+                                            className="mt-1 p-2 border rounded-md w-1/2 focus:outline-none focus:ring focus:border-blue-300"
+                                        />
+                                    ) : null}
+                                    {selection == "Provider" ? (
+                                        <input
+                                            type="number"
+                                            name="provider_id"
+                                            value={data.provider_id}
+                                            onChange={handleChange}
+                                            className="mt-1 p-2 border rounded-md w-1/2 focus:outline-none focus:ring focus:border-blue-300"
+                                        />
+                                    ) : null}
                                 </div>
-                                : null}
-                            {type == "invoice" ?
-                                <div>
-                                    <input type="number" name="user_id" value={data.user_id} onChange={handleChange} hidden />
+                            ) : null}
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex justify-center items-center">
+                                    <input
+                                        type="number"
+                                        name="user_id"
+                                        value={data.user_id}
+                                        onChange={handleChange}
+                                        hidden
+                                    />
                                 </div>
-                                : null}
-                            {type == "invoice" ?
-                                <div>
-                                    <label>Item Id: </label>
-                                    <input type="number" name="item_id" value={data.item_id} onChange={handleChange} />
+                            ) : null}
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex justify-center items-center">
+                                    <label className="block text-gray-700 mr-3  w-24 text-right">
+                                        Item Id:{" "}
+                                    </label>
+                                    <input
+                                        type="number"
+                                        name="item_id"
+                                        value={data.item_id}
+                                        onChange={handleChange}
+                                        className="mt-1 p-2 border rounded-md w-1/2 focus:outline-none focus:ring focus:border-blue-300"
+                                    />
                                 </div>
-                                : null}
-                            {type == "invoice" ?
-                                <div>
-                                    <label>Amount: </label>
-                                    <input type="number" name="amount" value={data.amount} onChange={handleChange} />
+                            ) : null}
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex justify-center items-center">
+                                    <label className="block text-gray-700 mr-3  w-24 text-right">
+                                        Amount:{" "}
+                                    </label>
+                                    <input
+                                        type="number"
+                                        name="amount"
+                                        value={data.amount}
+                                        onChange={handleChange}
+                                        className="mt-1 p-2 border rounded-md w-1/2 focus:outline-none focus:ring focus:border-blue-300"
+                                    />
                                 </div>
-                                : null}
-                            {type == "invoice" ?
-                                <div>
-                                    <label>Total Price: </label>
-                                    <input type="text" name="total_price" value={data.total_price} onChange={handleChange} disabled />
+                            ) : null}
+                            {type == "invoice" ? (
+                                <div className="mb-4 w-200 flex  justify-center items-center">
+                                    <label className="block text-gray-700 mr-3  w-24 text-right">
+                                        Total Price:{" "}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="total_price"
+                                        value={data.total_price}
+                                        onChange={handleChange}
+                                        disabled
+                                        className="mt-1 p-2 border rounded-md w-1/2 focus:outline-none focus:ring focus:border-blue-300"
+                                    />
                                 </div>
-                                : null}
-                            <div className="mb-4 w-200 flex justify-center items-center ">
+                            ) : null}
+                            <div className="mb-4 w-200 flex justify-center items-center">
                                 <button
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
                                     type="submit"
                                 >
                                     Save
